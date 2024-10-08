b7cfc6e47940ac7b82f279d3e46982aa60e5a1b4
GI000000 <giogio0570@gmail.com>
2024-10-08 21:10:30 +0400

    added site fundaments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Card from './components/Card';
+import Card from './components/card/Card';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <main>
-      <h1 className='main-header'>Flip cards!</h1>
-      <Card text="მისინფორმაცია" />
+      <Header />
+      <h1 className='header-1'>განივითარე მედიაწიგნიერება</h1>
+      <div className='macedonia-news'>
+        <img src='macedonia.jpeg' alt='ალექსანდრე მაკედონელი' />
+        <div className='text-container'>
+          <h2>ალექსანდრე მაკედონელი ქართველია!!!</h2>
+          <p>გამარჯობა, შენ თუ ახლა ამას კითხულობ, ეს ნიშნავს იმას რომ შენ მახეში გაები, სავარაუდოდ შემოგხვდა სტატია სახელწოდებით ,,ალექსანდრე მაკედონელი ქართველია", ამ თამამმა და არარეალურამ განაცხადმა მოახერხა შენი ამ სტატიაზე გადმოყვანა, რითაც შენ გახდი ყალბი ამბის, იგივე fake news-ის მსხვერპლი. შეიძლება...</p>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <header>
+        <ul>
+            <li>მთავარი</li>
+            <li>სიახლეები</li>
+            <li>შეამოწმე ცოდნა</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
